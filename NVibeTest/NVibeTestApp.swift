3ebceada21c38e2e1c97995934862ffed767d27b
Sadel Muwahed <sadeelmu@outlook.com>
2025-05-12 15:47:51 +0200

    Initial Commit

---
+++ NVibeTest/NVibeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NVibeTestApp.swift
+//  NVibeTest
+//
+//  Created by Sadeel Muwahed on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NVibeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
